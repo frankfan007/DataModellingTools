ec0b5339b51bdb743c8c13b23f6986278b56c655
Tommaso Fabbri <atfabbri@gmail.com>
2016-07-14 17:48:32 +0200

    DEV: mypy passed only warnings due to choice commented part - flake8 passed - pylint maybe

--- dmt/B_mappers/vdm_B_mapper.py
+++ dmt/B_mappers/vdm_B_mapper.py
@@ -29,46 +29,52 @@ It is used to model asynchronous processes (SDL)...
 '''
 
 # from ..commonPy.utility import panic
-from ..commonPy.asnAST import isSequenceVariable, sourceSequenceLimit, AsnBasicNode, AsnEnumerated
 
+from typing import List
+
+from ..commonPy.asnAST import (
+    sourceSequenceLimit, isSequenceVariable, AsnInt, AsnReal, AsnEnumerated,
+    AsnBool, AsnSequenceOrSet, AsnSequenceOrSetOf, AsnChoice, AsnOctetString,
+    AsnBasicNode, AsnNode, AsnSequence, AsnSet, AsnSetOf, AsnSequenceOf)
+from ..commonPy.asnParser import AST_Lookup, AST_Leaftypes
 from ..commonPy.recursiveMapper import RecursiveMapper
+
 from .asynchronousTool import ASynchronousToolGlueGenerator
 
 from . import c_B_mapper
 
 isAsynchronous = True
-rtdsBackend = None
 cBackend = None
 vdmBackend = None
 
 
-def Version():
+def Version() -> None:
     print("Code generator: " + "$Id: vdm_B_mapper.py 2390 2015-07-04 12:39:17Z tfabbri $")
 
 
 # noinspection PyListCreation
 # pylint: disable=no-self-use
 class FromVDMToASN1SCC(RecursiveMapper):
-    def __init__(self):
+    def __init__(self) -> None:
         self.uniqueID = 0
 
-    def UniqueID(self):
+    def UniqueID(self) -> int:
         self.uniqueID += 1
         return self.uniqueID
 
-    def DecreaseUniqueID(self):
+    def DecreaseUniqueID(self) -> None:
         self.uniqueID -= 1
 
-    def MapInteger(self, srcVDMVariable, destVar, _, __, ___):
+    def MapInteger(self, srcVDMVariable: str, destVar: str, _: AsnInt, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         return ["%s = (asn1SccSint)((TVP) %s)->value.intVal;\n" % (destVar, srcVDMVariable)]
 
-    def MapBoolean(self, srcVDMVariable, destVar, _, __, ___):
+    def MapBoolean(self, srcVDMVariable: str, destVar: str, _: AsnBool, __: AST_Leaftypes, ___: AST_Lookup)-> List[str]:  # pylint: disable=invalid-sequence-index
         return ["%s = (%s->value.boolVal==true)?0xff:0;\n" % (destVar, srcVDMVariable)]
 
-    def MapReal(self, srcVDMVariable, destVar, _, __, ___):
+    def MapReal(self, srcVDMVariable: str, destVar: str, _: AsnReal, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         return ["%s = ((TVP) %s)->value.doubleVal;\n" % (destVar, srcVDMVariable)]
 
-    def MapSequenceOf(self, srcVDMVariable, destVar, node, leafTypeDict, names):
+    def MapSequenceOf(self, srcVDMVariable: str, destVar: str, node: AsnSequenceOrSetOf, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         lines = []  # type: List[str]
         lines.append("{\n")
         uniqueId = self.UniqueID()
@@ -91,7 +97,7 @@ class FromVDMToASN1SCC(RecursiveMapper):
         self.DecreaseUniqueID()
         return lines
 
-    def MapOctetString(self, srcVDMVariable, destVar, node, __, ___):
+    def MapOctetString(self, srcVDMVariable: str, destVar: str, node: AsnOctetString, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         lines = []  # type: List[str]
         lines.append("{\n")
         lines.append("    int i;\n")
@@ -107,8 +113,8 @@ class FromVDMToASN1SCC(RecursiveMapper):
         lines.append("}\n")
         return lines
 
-    def MapEnumerated(self, srcVDMVariable, destVar, node, _, __):
-        lines = []
+    def MapEnumerated(self, srcVDMVariable: str, destVar: str, node: AsnEnumerated, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
+        lines = []  # type: List[str]
         lines.append("switch(%s) {\n" %srcVDMVariable)
         for m in node._members:
             lines.append("    case QUOTE_%s:\n" % m[0].upper())
@@ -117,7 +123,7 @@ class FromVDMToASN1SCC(RecursiveMapper):
         lines.append("}\n")
         return lines
 
-    def MapSequence(self, srcVDMVariable, destVar, node, leafTypeDict, names):
+    def MapSequence(self, srcVDMVariable: str, destVar: str, node: AsnSequenceOrSet, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         lines = []  # type: List[str]
         for child in node._members:
             lines.extend(
@@ -129,7 +135,7 @@ class FromVDMToASN1SCC(RecursiveMapper):
                     names))
         return lines
 
-    def MapSet(self, srcVDMVariable, destVar, node, leafTypeDict, names):
+    def MapSet(self, srcVDMVariable: str, destVar: str, node: AsnSequenceOrSet, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         return self.MapSequence(srcVDMVariable, destVar, node, leafTypeDict, names)  # pragma: nocover
 '''
     def MapChoice(self, srcVDMVariable, destVar, node, leafTypeDict, names):
@@ -158,26 +164,26 @@ class FromVDMToASN1SCC(RecursiveMapper):
 # noinspection PyListCreation
 # pylint: disable=no-self-use
 class FromASN1SCCtoVDM(RecursiveMapper):
-    def __init__(self):
+    def __init__(self) -> None:
         self.uniqueID = 0
 
-    def UniqueID(self):
+    def UniqueID(self) -> int:
         self.uniqueID += 1
         return self.uniqueID
 
-    def DecreaseUniqueID(self):
+    def DecreaseUniqueID(self) -> None:
         self.uniqueID -= 1
 
-    def MapInteger(self, srcVar, dstVDMVariable, _, __, ___):
+    def MapInteger(self, srcVar: str, dstVDMVariable: str, _: AsnInt, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         return ["%s = newInt(%s);\n" % (dstVDMVariable, srcVar)]
 
-    def MapBoolean(self, srcVar, dstVDMVariable, _, __, ___):
+    def MapBoolean(self, srcVar: str, dstVDMVariable: str, _: AsnBool, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         return ["%s = (%s)?newBool(true):newBool(false);\n" % (dstVDMVariable, srcVar)]
 
-    def MapReal(self, srcVar, dstSDLVariable, _, __, ___):
-        return ["%s = newReal(%s);\n" % (dstSDLVariable, srcVar)]
+    def MapReal(self, srcVar: str, dstVDMVariable: str, _: AsnReal, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
+        return ["%s = newReal(%s);\n" % (dstVDMVariable, srcVar)]
 
-    def MapSequenceOf(self, srcVar, dstVDMVariable, node, leafTypeDict, names):
+    def MapSequenceOf(self, srcVar: str, dstVDMVariable: str, node: AsnSequenceOrSetOf, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         lines = []  # type: List[str]
         lines.append("{\n")
         uniqueId = self.UniqueID()
@@ -198,9 +204,7 @@ class FromASN1SCCtoVDM(RecursiveMapper):
         self.DecreaseUniqueID()
         return lines
 
-    def MapOctetString(self, srcVar, dstVDMVariable, node, _, __):
-        # for i in xrange(0, node._range[-1]):
-        #     lines.append("%s[%d] = %s->buf[%d];\n" % (dstSDLVariable, i, srcVar, i))
+    def MapOctetString(self, srcVar: str, dstVDMVariable: str, node: AsnOctetString, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         lines = []  # type: List[str]
         limit = sourceSequenceLimit(node, srcVar)
         lines.append("{\n")
@@ -212,8 +216,8 @@ class FromASN1SCCtoVDM(RecursiveMapper):
         lines.append("}\n")
         return lines
 
-    def MapEnumerated(self, srcVar, dstVDMVariable, node, _, __):
-        lines = []
+    def MapEnumerated(self, srcVar: str, dstVDMVariable: str, node: AsnEnumerated, __: AST_Leaftypes, ___: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
+        lines = []  # type: List[str]
         lines.append("switch(%s) {\n" %srcVar)
         for m in node._members:
             lines.append("    case %s:\n" % m[1])
@@ -222,7 +226,7 @@ class FromASN1SCCtoVDM(RecursiveMapper):
         lines.append("}\n")
         return lines
 
-    def MapSequence(self, srcVar, dstVDMVariable, node, leafTypeDict, names):
+    def MapSequence(self, srcVar: str, dstVDMVariable: str, node: AsnSequenceOrSet, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         lines = []  # type: List[str]
         for child in node._members:
             lines.extend(
@@ -234,7 +238,7 @@ class FromASN1SCCtoVDM(RecursiveMapper):
                     names))
         return lines
 
-    def MapSet(self, srcVar, dstSDLVariable, node, leafTypeDict, names):
+    def MapSet(self, srcVar: str, dstSDLVariable: str, node: AsnSequenceOrSet, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> List[str]:  # pylint: disable=invalid-sequence-index
         return self.MapSequence(srcVar, dstSDLVariable, node, leafTypeDict, names)  # pragma: nocover
 '''
     def MapChoice(self, srcVar, dstSDLVariable, node, leafTypeDict, names):
@@ -262,22 +266,22 @@ class FromASN1SCCtoVDM(RecursiveMapper):
 
 
 class VDM_GlueGenerator(ASynchronousToolGlueGenerator):
-    def __init__(self):
+    def __init__(self) -> None:
         ASynchronousToolGlueGenerator.__init__(self)
         self.FromVDMToASN1SCC = FromVDMToASN1SCC()
         self.FromASN1SCCtoVDM = FromASN1SCCtoVDM()
 
-    def Version(self):
+    def Version(self) -> None:
         print("Code generator: " + "$Id: vdm_B_mapper.py 2390 2015-07-04 12:39:17Z tfabbri $")
 
-    def HeadersOnStartup(self, unused_asnFile, unused_outputDir, unused_maybeFVname):
+    def HeadersOnStartup(self, unused_asnFile: str, unused_outputDir: str, unused_maybeFVname: str) -> None:
         print(unused_asnFile)
 
         self.C_HeaderFile.write("#include <assert.h>\n\n")
         self.C_HeaderFile.write("#include \"%s.h\"\n" % self.asn_name)
         self.C_HeaderFile.write("#include \"Vdm.h\"\n\n")
 
-    def Encoder(self, nodeTypename, node, leafTypeDict, names, encoding):
+    def Encoder(self, nodeTypename: str, node: AsnNode, leafTypeDict: AST_Leaftypes, names: AST_Lookup, encoding: str) -> None:
         if encoding.lower() in ["uper", "acn"]:
             return
         fileOutHeader = self.C_HeaderFile
@@ -314,7 +318,7 @@ class VDM_GlueGenerator(ASynchronousToolGlueGenerator):
         fileOutSource.write("\n".join(lines))
         fileOutSource.write("\n}\n\n")
 
-    def Decoder(self, nodeTypename, node, leafTypeDict, names, encoding):
+    def Decoder(self, nodeTypename: str, node: AsnNode, leafTypeDict: AST_Leaftypes, names: AST_Lookup, encoding: str) -> None:
         if encoding.lower() in ["uper", "acn"]:
             return
         fileOutHeader = self.C_HeaderFile
@@ -349,7 +353,7 @@ class VDM_GlueGenerator(ASynchronousToolGlueGenerator):
         fileOutSource.write("\n}\n\n")
 
 
-def OnStartup(modelingLanguage, asnFile, outputDir, maybeFVname, useOSS):
+def OnStartup(modelingLanguage: str, asnFile: str, outputDir: str, maybeFVname: str, useOSS: bool) -> None:
     global vdmBackend
     vdmBackend = VDM_GlueGenerator()
     vdmBackend.OnStartup(modelingLanguage, asnFile, outputDir, maybeFVname, useOSS)
@@ -358,41 +362,41 @@ def OnStartup(modelingLanguage, asnFile, outputDir, maybeFVname, useOSS):
     cBackend.OnStartup("C", asnFile, outputDir, maybeFVname, useOSS)
 
 
-def OnBasic(nodeTypename, node, leafTypeDict, names):
+def OnBasic(nodeTypename: str, node: AsnBasicNode, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnBasic(nodeTypename, node, leafTypeDict, names)
     cBackend.OnBasic(nodeTypename, node, leafTypeDict, names)
 
 
-def OnSequence(nodeTypename, node, leafTypeDict, names):
+def OnSequence(nodeTypename: str, node: AsnSequence, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnSequence(nodeTypename, node, leafTypeDict, names)
     cBackend.OnSequence(nodeTypename, node, leafTypeDict, names)
 
 
-def OnSet(nodeTypename, node, leafTypeDict, names):
+def OnSet(nodeTypename: str, node: AsnSet, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnSet(nodeTypename, node, leafTypeDict, names)  # pragma: nocover
     cBackend.OnSet(nodeTypename, node, leafTypeDict, names)  # pragma: nocover
 
 
-def OnEnumerated(nodeTypename, node, leafTypeDict, names):
+def OnEnumerated(nodeTypename: str, node: AsnEnumerated, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnEnumerated(nodeTypename, node, leafTypeDict, names)
     cBackend.OnEnumerated(nodeTypename, node, leafTypeDict, names)
 
 
-def OnSequenceOf(nodeTypename, node, leafTypeDict, names):
+def OnSequenceOf(nodeTypename: str, node: AsnSequenceOf, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnSequenceOf(nodeTypename, node, leafTypeDict, names)
     cBackend.OnSequenceOf(nodeTypename, node, leafTypeDict, names)
 
 
-def OnSetOf(nodeTypename, node, leafTypeDict, names):
+def OnSetOf(nodeTypename: str, node: AsnSetOf, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnSetOf(nodeTypename, node, leafTypeDict, names)  # pragma: nocover
     cBackend.OnSetOf(nodeTypename, node, leafTypeDict, names)  # pragma: nocover
 
 
-def OnChoice(nodeTypename, node, leafTypeDict, names):
+def OnChoice(nodeTypename: str, node: AsnChoice, leafTypeDict: AST_Leaftypes, names: AST_Lookup) -> None:
     vdmBackend.OnChoice(nodeTypename, node, leafTypeDict, names)
     cBackend.OnChoice(nodeTypename, node, leafTypeDict, names)
 
 
-def OnShutdown(modelingLanguage, asnFile, maybeFVname):
+def OnShutdown(modelingLanguage: str, asnFile: str, maybeFVname: str) -> None:
     vdmBackend.OnShutdown(modelingLanguage, asnFile, maybeFVname)
     cBackend.OnShutdown("C", asnFile, maybeFVname)
