d608c6c4e9c3690c663f6cc2e6050597a79f290c
Tommaso Fabbri <atfabbri@gmail.com>
2017-12-20 17:13:39 +0100

    p - patch correct the way of executing

--- dmt/vdm_tests/INSTALL.sh
+++ dmt/vdm_tests/INSTALL.sh
@@ -13,8 +13,13 @@ if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     if [ -e ${HOME}/.profile ]; then 
         touch "${HOME}/.profile"
     fi
-    sudo apt-get install libxslt1-dev libxml2-dev zlib1g-dev python3-pip git python-antlr
+    sudo apt-get update
     sudo apt-get install mono-devel fsharp antlr
+    sudo apt-get install libxslt1-dev libxml2-dev zlib1g-dev python3-pip git python-antlr
+    wget -O -q https://github.com/ttsiodras/DataModellingTools/files/335591/antlr-2.7.7.tar.gz | targ -zxvf
+    cd antlr-2.7.7/lib/python 
+    sudo pip2 install .
+
 elif [ "$(uname)" == "Darwin" ]; then
     BASHRC=".bash_profile"
     brew update
@@ -22,27 +27,29 @@ elif [ "$(uname)" == "Darwin" ]; then
     brew install libxslt python python3 lzlib binutils libantlr3c wget
     wget -O -q https://github.com/ttsiodras/DataModellingTools/files/335591/antlr-2.7.7.tar.gz | targ -zxvf
     cd antlr-2.7.7/lib/python 
-    pip2 install .
+    sudo pip2 install .
 fi
 
 # ASN.1 compiler 
 git clone https://github.com/ttsiodras/asn1scc
 cd asn1scc 
 xbuild /p:TargetFrameworkVersion="v4.5"
+PWD=$(pwd)
 mv "$(pwd)/Asn1f2/bin/Debug/Asn1f2.exe" "$(pwd)/Asn1f2/bin/Debug/asn1.exe"
-pathadd "$(pwd)/Asn1f2/bin/Debug"  
-echo 'export VDM_STG=$(pwd)/Asn1f2/Debug/bin/vdm.stg' >> $HOME/.profile
+pathadd "${PWD}/Asn1f2/bin/Debug"  
+echo "export VDM_STG=${PWD}/Asn1f2/Debug/bin/vdm.stg" >> $HOME/.profile
 cd ../
 
 # VDM-mapper repository clone
 git clone -b vdm-b-mapper https://github.com/tfabbri/DataModellingTools
 cd DataModellingTools
+PWD=$(pwd)
 pip3 install --user --upgrade .
 pathadd "${HOME}/.local/bin"
-pathadd "$(pwd)/dmt/vdm_tests/"
+pathadd "${PWD}/dmt/vdm_tests/"
 cd ../
 
-if grep -q "source .profile" $HOME/$BASHRC; then 
+if ! grep -q "source .profile" $HOME/$BASHRC; then 
     echo 'source .profile' >> $HOME/$BASHRC
 fi
 echo "Restart your shell to complete the installation" 
