dc41bd7169a506e9c59631a3a989f527b5bec912
Tommaso Fabbri <atfabbri@gmail.com>
2017-12-27 11:44:02 +0100

    p - possibile fix to installation procedure of Mapping functions

--- dmt/vdm_tests/INSTALL.sh
+++ dmt/vdm_tests/INSTALL.sh
@@ -2,13 +2,40 @@
 # Utility function 
 pathadd() {
     if [ -d "$1" ] && [[ ":$PATH" != *":$1:"* ]]; then
-        echo 'export PATH=$PATH:'${1} >> $HOME/.profile
+        echo 'export PATH=$PATH:'${1} >> $HOME/$BASHRC
     fi
 }
 
-# Dependencies
-BASHRC=".bashrc"
+OPTIND=1
+install_dir=""
 
+
+while getopts "h?vf:" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Installation of mapping functions support"
+        exit 0
+        ;;
+    f)  INSTALL_DIR=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
+if [ ! "${INSTALL_DIR}" ]
+then
+    echo "OVERTURE INSTALL_DIR ARGUMENT EMPTY"
+    exit 1
+fi
+
+echo "INSTALL_DIR=${install_dir}" 
+
+BASHRC=".bashrc"
+cd ${INSTALL_DIR}
+
+# Dependencies
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then 
     if [ -e ${HOME}/.profile ]; then 
         touch "${HOME}/.profile"
@@ -37,7 +64,7 @@ xbuild /p:TargetFrameworkVersion="v4.5"
 PWD=$(pwd)
 mv "$(pwd)/Asn1f2/bin/Debug/Asn1f2.exe" "$(pwd)/Asn1f2/bin/Debug/asn1.exe"
 pathadd "${PWD}/Asn1f2/bin/Debug"  
-echo "export VDM_STG=${PWD}/Asn1f2/Debug/bin/vdm.stg" >> $HOME/.profile
+echo "export VDM_STG=${PWD}/Asn1f2/Debug/bin/vdm.stg" >> $HOME/$BASHRC
 cd ../
 
 # VDM-mapper repository clone
@@ -49,7 +76,4 @@ pathadd "${HOME}/.local/bin"
 pathadd "${PWD}/dmt/vdm_tests/"
 cd ../
 
-if ! grep -q "source .profile" $HOME/$BASHRC; then 
-    echo 'source .profile' >> $HOME/$BASHRC
-fi
-echo "Restart your shell to complete the installation" 
+echo "Restart your shell to complete the installation"
